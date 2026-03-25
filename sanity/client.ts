14fa4743fc9f83d63fba4cbcd91039d3229102a4
Justine Kem's <justinekems@site.com>
2026-03-25 20:58:13 +0100

    fix: Provide fallback Sanity projectId for Vercel build

--- sanity/client.ts
+++ sanity/client.ts
@@ -1,9 +1,12 @@
 import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'd8v5zxvs'
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId,
+  dataset,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-03-18',
   useCdn: process.env.NODE_ENV === 'production',
   token: process.env.SANITY_API_TOKEN,
